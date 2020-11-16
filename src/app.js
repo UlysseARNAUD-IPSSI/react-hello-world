5278ee6940ca1e98fd12dd76c8eb93acd5a48b4d
Ulysse ARNAUD <mexanga@gmail.com>
2020-11-16 15:30:37 +0100

    Chapitre 4 : Evenement - Renommage de Incrementer en ManuelIncrementer - Ajout d'un bouton avec onClick appellant la fonction permettant de mettre sur pause - Ajout d'un prop paused permettant de definir la valeur au démarrage de l'incrementation - Ajout d'un state paused permettant de définir si l'incrementation est sur pause ou non

--- src/app.js
+++ src/app.js
@@ -1,44 +1,58 @@
-class Incrementer extends React.Component {
+class ManuelIncrementer extends React.Component {
 
     static defaultProps = {
         start: 0,
-        step: 1
+        step: 1,
+        paused: false
     }
 
     constructor(props) {
         super(props);
 
-        const {start} = props;
+        const {start,paused} = props;
 
-        this.state = { value: start }
+        this.state = { value: start, paused }
 
         this.timer = null
     }
 
     render() {
         const {start} = this.props
-        const {value} = this.state
+        const {value, paused} = this.state
         return (
             <div>
                 <p>Début : {start}</p>
                 <p>Valeur actuelle : {value}</p>
+                <button onClick={this.pause.bind(this)}>{paused ? "Reprendre" : "Pause"}</button>
             </div>
         )
     }
 
     componentDidMount() {
-        this.timer = window.setInterval(() => this.tick(), 1000)
+        this.timer = window.setInterval(() => {
+            return this.increment()
+        }, 1000)
     }
 
     componentWillUnmount() {
         window.clearInterval(this.timer)
     }
 
-    tick() {
+    increment() {
+        const {paused} = this.state
+
+        if (true === paused) return
+
         this.setState((state,props) => ({
             value: state.value + props.step
         }))
     }
+
+    pause() {
+        this.setState((state,props) => ({
+            paused: !state.paused
+        }))
+    }
 }
 
-ReactDOM.render(<Incrementer/>, document.querySelector("#app"))
+ReactDOM.render(<ManuelIncrementer/>, document.querySelector("#app"))
